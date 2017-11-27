6809dde1f706c3ab2454919c01ac6384451c99c3
Felix Moon <daekeun.moon@gmail.com>
2017-11-27 09:18:51 -0600

    Added Ryan's new chat as a modeless dialog

--- public/js/new_chat.js
+++ public/js/new_chat.js
@@ -229,7 +229,7 @@ $(function() {
   socket.on('login', function (data) {
     connected = true;
     // Display the welcome message
-    var message = "Welcome to Socket.IO Chat – ";
+    var message = "Welcome to The Brain Dump Chatroom ";
     log(message, {
       prepend: true
     });
