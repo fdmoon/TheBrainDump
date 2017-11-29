afcf7d5bc67770cbe0664dbaeb3e3ac4a3265c79
Ryan Hayden <ryanhayden@ryans-mbp.home>
2017-11-28 22:14:56 -0600

    linked the comments

--- public/js/sign_up.js
+++ public/js/sign_up.js
@@ -3,10 +3,14 @@ $(document).ready(function() {
   $("#submit").on("click", function() {
     var userData = {
       username: $("#usernameInput").val().trim(),
-      password: $("#passwordInput").val().trim()
+      password: $("#passwordInput").val().trim(),
+      terms:    $('input[type=checkbox]').prop('checked')
     };
 
-    if (!userData.username || !userData.password) {
+    if (!userData.terms) {
+      alert("Please indicate that you have read and agree to the Terms and Conditions and Privacy Policy");
+      return;
+    } else if (!userData.username || !userData.password) {
       return;
     }
 
@@ -15,14 +19,24 @@ $(document).ready(function() {
       // If there's an error, handle it by throwing up a boostrap alert
     }).catch(function(err) {
       console.log(err.responseJSON);
+      // alert(err.responseJSON.errors[0].message);
+      if (err.responseJSON.errors[0].message === "username must be unique") {
+        $("#usernameInput").addClass("is-danger");
+        $("#note").text(err.responseJSON.errors[0].message);
+      }
       // $("#alert .msg").text(err.responseJSON);
       // $("#alert").fadeIn(500);
     });
 
     $("#usernameInput").val("");
-    $("#passwordInput").val("");    
+    $("#passwordInput").val("");
 
   });
+
+  // Cancel button is pressed
+  document.getElementById("cancelme").onclick = function () {
+      location.reload();
+  };
 
 });
 
--- server.js
+++ server.js
@@ -50,6 +50,8 @@ require("./controllers/api-routes.js")(app);
 require("./controllers/live_chat.js")(io);
 require("./controllers/new_chat.js")(io);
 
+require("./controllers/aws-routes.js")(app);
+
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ /*force: true*/ }).then(function() {
