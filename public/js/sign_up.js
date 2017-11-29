19ae8a5fbf6ae27a035ff4399d0211b77f938e98
annejsize <annejsize@gmail.com>
2017-11-28 21:27:42 -0600

    added modal form

--- public/js/sign_up.js
+++ public/js/sign_up.js
@@ -37,6 +37,21 @@ $(document).ready(function() {
   document.getElementById("cancelme").onclick = function () {
       location.reload();
   };
+  // Modal with Terms and Conditions outlined
+  document.querySelector('a#open-modal').addEventListener('click', function(event) {
+    event.preventDefault();
+        var modal = document.querySelector('.modal');  // assuming you have only 1
+        var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+
+    modal.querySelector('.modal-close').addEventListener('click', function(e) {
+      e.preventDefault();
+      modal.classList.remove('is-active');
+      html.classList.remove('is-clipped');
+
+    });
+  });
+
 
 });
-
